5a87056d40690230f6317f55d59a86a9668e397a
kvsg45 <98922762+kvsg45@users.noreply.github.com>
2023-05-01 00:20:48 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-# Install secrets
-pip3 install secrets
+# Install OpenSSL
+pip3 install pyopenssl
+
 
 # Install datetime
 pip3 install datetime
 
-# Install threading
-pip3 install threading
-
-# Install re
-pip3 install re
-
-# Install OpenSSL
-pip3 install pyopenssl
 
 # Update pip and setup tools
 pip3 install --upgrade pip setuptools
